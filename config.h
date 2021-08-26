05d5b0c75bfd46415ba3da88c38d6d72de42c33b
Lester Lo <21245380+lesterlo@users.noreply.github.com>
2021-08-26 21:56:55 +0800

    Improve the code quality

--- config.h
+++ config.h
@@ -1,12 +1,21 @@
 //Constant
 
-#define LED_PWM_HIGH_BOUND 0  //pwm value for max brightness
+#define LED_PWM_HIGH_BOUND 200  //pwm value for max brightness
 #define LED_PWM_LOW_BOUND 255 //pwm value for min brightness   
+
+
+//OLED display parameter
+#define LED_BRIGHTNESS_FONT_XPOS 15
+#define LED_BRIGHTNESS_FONT_YPOS 5
+
 #define LED_METER_BAR_XPOS 7
 #define LED_METER_BAR_YPOS 25
 #define LED_METER_BAR_WIDTH 110
-#define LED_METER_BAR_CURSOR_W 3 //Height
+#define LED_METER_BAR_HEIGHT 1
+#define LED_METER_BAR_CURSOR_W 3 //Width
 #define LED_METER_BAR_CURSOR_H 7 //Height
+#define LED_METER_BAR_SEPERATION_W 1 //Width
+#define LED_METER_BAR_SEPERATION_H LED_METER_BAR_CURSOR_H //Height
 
 // PIN map
 #define ENCODER_S1_PIN 2 //Define you encoder connection pin Here
--- led-light-box.ino
+++ led-light-box.ino
@@ -22,6 +22,7 @@ Adafruit_SSD1306 oled(128, 32, &Wire, -1);//OLED 128x64 No Reset Pin
 
 int led_brightness = 10; //CLip to 0-100%
 bool light_status = false;
+bool reset_brightness_flag = false;
 
 
 //Timer isr
@@ -30,6 +31,7 @@ void isr_mstimer2()
     enc.btn_task();
 }
 
+//Set the easter egg
 void showBitmap(void) {
   oled.clearDisplay();
   oled.drawBitmap(10, 0, pig_rabbit_logoBitmap, 128, 32, WHITE);
@@ -47,15 +49,14 @@ void setup() {
     oled.clearDisplay();
     oled.display();
 
+    //Setup the Pin Mode
     pinMode(LED_EN_PIN, OUTPUT);
     pinMode(LED_PIN, OUTPUT);
     
 
-  // Endcoder setup
+    // Endcoder setup
 
-  // Set the button monitoring parameter
-  // Commented code for using default interval in this example
-
+    // Set the button monitoring parameter
     enc.setBtnDebounceInterval(150);
     enc.setBtnPressInterval(500);
     enc.setBtnHoldInterval(1000);
@@ -70,16 +71,15 @@ void loop() {
   
     NSEncoder_btn::BTN_STATE enc_btn;
   
-    int enc_diff = 0;
+    int enc_diff = 0; //The encoder diff counter
     // Handle Brightness
     if((enc_diff = enc.get_diffPosition()) != 0) //Get the position diff value
     {
-        led_brightness += enc_diff; // Add diff
+        led_brightness += enc_diff; // Add the encoder position diff
 
-    //Clip the led value to 0-100%
+        //Clip the led value to 0-100%
         led_brightness = led_brightness > 100 ? 100 : led_brightness;
         led_brightness = led_brightness <  0  ?  0 : led_brightness;
-
     }
 
     //Write the PWM value to the LED
@@ -87,7 +87,7 @@ void loop() {
 
 
     //Button Jobs
-    enc_btn = enc.get_Button();
+    enc_btn = enc.get_Button(); //Get the button status
     switch(enc_btn)
     {
         case NSEncoder_btn::OPEN:
@@ -111,28 +111,39 @@ void loop() {
         break;
 
         case NSEncoder_btn::RELEASED:
-        led_brightness = 50;
+            //Reset the brightness to 50%
+            led_brightness = 50; //DO the reset procedure
+            reset_brightness_flag = false;
         break;
     
         case NSEncoder_btn::HELD:
-
+            //Recognize the RESET action 
+            reset_brightness_flag = true;
         break;
         
         case NSEncoder_btn::DOUBLE_PRESSED:
-        showBitmap();
+            showBitmap();
         break;
     }
     
     // OLED Jobs
-    oled.clearDisplay();
+    oled.clearDisplay(); //Clear the display first
     
     //Show the LED Brightness
     oled.setTextSize(2); // Draw 2X-scale text
     oled.setTextColor(SSD1306_WHITE);
-    oled.setCursor(10, 5);
-    oled.print(F("LED: "));
-    oled.print(led_brightness);
-    oled.print(F("%"));
+    oled.setCursor(LED_BRIGHTNESS_FONT_XPOS, LED_BRIGHTNESS_FONT_YPOS);
+    //Show the message
+    if(reset_brightness_flag)
+    {   //Show the reset status
+        oled.print(F("  RESET "));
+    }
+    else
+    {   //Show the LED brightness value
+        oled.print(F("LED: "));
+        oled.print(led_brightness);
+        oled.print(F("%"));
+    }
 
     //Draw the oled outbound, show when led is disabled
     if(light_status == false)
@@ -140,16 +151,16 @@ void loop() {
 
 
     //Draw the brightness meter bar
-    oled.fillRect(7, 25, 110 , 1, SSD1306_WHITE);
+    oled.fillRect(LED_METER_BAR_XPOS, LED_METER_BAR_YPOS, LED_METER_BAR_WIDTH, LED_METER_BAR_HEIGHT, SSD1306_WHITE);
 
-    // Draw the seperation
+    // Draw the meter bar seperation
     for(int i=0; i<=100; i+=20)
     {
         oled.fillRect(
             map(i,0,100,LED_METER_BAR_XPOS,LED_METER_BAR_XPOS+LED_METER_BAR_WIDTH), 
             LED_METER_BAR_YPOS-LED_METER_BAR_CURSOR_H/2, 
-            1,
-            LED_METER_BAR_CURSOR_H, 
+            LED_METER_BAR_SEPERATION_W,
+            LED_METER_BAR_SEPERATION_H, 
             SSD1306_WHITE);
     }
 
@@ -161,5 +172,6 @@ void loop() {
         LED_METER_BAR_CURSOR_H, 
         SSD1306_WHITE);
 
+    //Display the OLED drawing
     oled.display();
 }
